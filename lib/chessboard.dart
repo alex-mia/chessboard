95189b807f6407431a7bc74180365137ada9c93b
alex-mia <a.mukalov@ro.ru>
2022-07-01 15:14:44 +0300

    Initial commit

---
+++ lib/chessboard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ChessPage extends StatefulWidget {
+  const ChessPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChessPage> createState() => _ChessPageState();
+}
+
+class _ChessPageState extends State<ChessPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
